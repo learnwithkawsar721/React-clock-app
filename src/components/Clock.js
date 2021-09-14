1049ae5164ff88b755a8a4b618b68b9570dc0551
learnwithkawsar <kawsar721g@gmail.com>
2021-09-14 20:54:41 +0600

    React Class component create and clock project

---
+++ src/components/Clock.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class Clock extends React.Component{
+    /* যদি propos এর প্রয়োজন হয় তাহলে consttuctor লিখতে হবে । আর যদি প্রয়োজন না হয় তাহলে লিখতে হবে না।  */
+//    constructor(props){
+//        super(props);
+//        this.state ={date:new Date()};
+//    }
+  /* যদি propos এর প্রয়োজন না হয় তাহলে consttuctor লিখতে হবে না। */
+   state = {date:new Date()};
+
+   componentDidMount(){
+       this.timeOut =setInterval(()=>this.tick(),1000);
+   }
+   componentWillUnmount(){
+       clearInterval(this.timeOut);
+   }
+   tick(){
+        /* component এর মান change করতে হলে setState() লিখতে হবে। এবং নতুন মান সেট করতে হবে।  */
+    this.setState({
+        date:new Date()
+    })
+   }
+   /* Class component এ rander() মেথড লিখতে হয়। এই মেথড কম্পনেন্ট কে re-use করে।  */
+    render(){
+      return (
+        <h1 className="text">
+            {this.state.date.toLocaleTimeString(this.props.local)}
+        </h1>
+      );
+    }
+}
+
+export default Clock;
