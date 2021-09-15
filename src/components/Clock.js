fb5deb8d4ead6936cab2a394805d0db2675a5b81
learnwithkawsar <kawsar721g@gmail.com>
2021-09-15 12:04:38 +0600

    Button handeling

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 class Clock extends React.Component{
     /* যদি propos এর প্রয়োজন হয় তাহলে consttuctor লিখতে হবে । আর যদি প্রয়োজন না হয় তাহলে লিখতে হবে না।  */
-//    constructor(props){
-//        super(props);
-//        this.state ={date:new Date()};
-//    }
+  //  constructor(props){
+  //      super(props);
+  //      this.state ={date:new Date(),local:'bn-Bd'};
+  //      this.hendelClick = this.hendelClick.bind(this);
+  //  }
   /* যদি propos এর প্রয়োজন না হয় তাহলে consttuctor লিখতে হবে না। */
-   state = {date:new Date()};
+   state = {
+     date:new Date(),
+     local:'bn-Bd'
+    };
 
    componentDidMount(){
        this.timeOut =setInterval(()=>this.tick(),1000);
@@ -21,15 +25,23 @@ class Clock extends React.Component{
         date:new Date()
     })
    }
+   // hendelClick button 
+   hendelClick(local){
+     this.setState({
+       local,
+     })
+   }
    /* Class component এ rander() মেথড লিখতে হয়। এই মেথড কম্পনেন্ট কে re-use করে।  */
     render(){
+      const {date,local}=this.state;
       return (
        <div className="clock">
           <div className="text-center">
           <h1 className="text">First React Clock App By <span>Kawsar</span></h1>
             <h1 className="text">
-                 {this.state.date.toLocaleTimeString(this.props.local)}
+                 {date.toLocaleTimeString(local)}
             </h1>
+            <button className="btn" onClick={this.hendelClick.bind(this,'en-US')}>Click her</button>
           </div>
        </div>
       );
